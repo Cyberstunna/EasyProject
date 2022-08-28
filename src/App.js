8dce117e9f33c27a9357b71309ef08e434e381fe
Nennii <nenniicallyntete@gmail.com>
2022-08-28 00:02:14 -0600

    Page Routing Initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import UserDashboard from "./pages/UserDashboard";
 import "./App.css"
 import NavigationBar from "./components/NavigationBar";
+import UserSettings from "./pages/UserSettings";
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
           <NavigationBar />
         </div>
         <div className="pl-24">
-          <UserDashboard />
+        <Routes>
+          <Route path="/" element={<UserDashboard />} />
+          <Route path="/settings" element={<UserSettings />} />
+        </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/UserSettings.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserSettings = () => {
+    return(
+        <div>
+            <h1>Hello from UserSettings</h1>
+        </div>
+    )
+}
+
+export default UserSettings;
