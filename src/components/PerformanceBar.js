e1f1e80ee8ce4bc1533b9f9b0a53b9fbb141814e
Nennii <nenniicallyntete@gmail.com>
2022-10-10 23:13:37 -0600

    Fixed syntaxes and ToDo list completed

--- src/components/PerformanceBar.js
+++ src/components/PerformanceBar.js
@@ -17,8 +17,8 @@ const PerformanceBar = () => {
                         <div className="flex flex-col w-fit">
                             <div className="flex flex-row font-extrabold w-fit px-1 text-sm rounded-xl bg-green-500 text-white">
                                 <p>5.6%</p>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                                 </svg>
                             </div>
                             <div>
@@ -26,8 +26,8 @@ const PerformanceBar = () => {
                             </div>
                         </div>
                         <div className="w-fit text-slate-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
                             </svg>
                         </div>
                         </div>
@@ -41,8 +41,8 @@ const PerformanceBar = () => {
                             <div className="flex flex-col w-fit">
                                 <div className="flex flex-row font-extrabold w-fit px-1 text-sm rounded-xl bg-red-400 text-white">
                                     <p>2.4%</p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6L9 12.75l4.286-4.286a11.948 11.948 0 014.306 6.43l.776 2.898m0 0l3.182-5.511m-3.182 5.51l-5.511-3.181" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6L9 12.75l4.286-4.286a11.948 11.948 0 014.306 6.43l.776 2.898m0 0l3.182-5.511m-3.182 5.51l-5.511-3.181" />
                                     </svg>
                                 </div>
                                 <div>
@@ -50,8 +50,8 @@ const PerformanceBar = () => {
                                 </div>
                             </div>
                             <div className="w-fit text-slate-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
                                 </svg>
                             </div>
                         </div>
@@ -73,8 +73,8 @@ const PerformanceBar = () => {
                         <li className="marker:w-fit h-fit w-full m-0">
                             <div className="flex flex-row m-0 align-center">
                                 <div className="pt-1 pr-2 md:pr-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                                     </svg>
                                 </div>
                                 <div>
@@ -88,8 +88,8 @@ const PerformanceBar = () => {
                         <li className="marker:w-fit h-fit w-full m-0">
                             <div className="flex flex-row m-0 align-center">
                                 <div className="pt-1 pr-2 md:pr-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                                     </svg>
                                 </div>
                                 <div>
@@ -103,8 +103,8 @@ const PerformanceBar = () => {
                         <li className="marker:w-fit h-fit w-full m-0">
                             <div className="flex flex-row m-0 align-center">
                                 <div className="pt-1 pr-2 md:pr-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                                     </svg>
                                 </div>
                                 <div>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -13,40 +13,57 @@ const ToDo = () => {
         .catch((error)=>{
             console.log(error)
         })
-    },[]);
+    },[userToDos, userId]);
 
     return(
-        <div className="flex flex-row px-3 pt-1 text-left place-content content-around w-5/6 h-96 shadow-2xl rounded-xl border-solid border-black bg-white divide-x divide-slate-100">
-            <div className="w-96 text-xl font-bold text-indigo-500">
+        <div className="px-3 pt-1 text-left w-5/6 h-96 shadow-2xl rounded-2xl border-solid border-black bg-white">
+            <div className="w-full text-xl font-bold text-indigo-500 flex flex-col place-content-between">
                 <div className="flex flex-row place-content-between">
                     <h2>To-Do List</h2>
                 </div>
-                <div className="flex flex-col">
-                    {userToDos.map((toDo)=>{
-                        return <ListItem  item={toDo} />
-                    })}
+                <div className="flex flex-col overflow-y-auto overflow-x-clip w-5/6 max-h-72 m-auto">
+                    { userToDos.map((toDo)=>{
+                            return <ListItem  item={toDo} user={userId} setUserToDos={setUserToDos} />
+                        })
+                    }
+                </div>
+                <div className="absolute bottom-0 py-3">
+                    <span className="text-xs">Outstanding Tasks: {userToDos.length}</span>
                 </div>
             </div>
         </div>
     )
 }
 
-const ListItem = ({item}) => {
+const ListItem = ({item, user, setUserToDos}) => {
 
     const checkedBox = (event) =>{
         if(event.target.checked){
             console.log('Task was completed');
-            axios.patch(`http://localhost:8080/todo/${event.target.value}/done`);
+            axios.patch(`http://localhost:8080/todo/${event.target.value}/done`, {user: user})
+            .then(({data})=>{
+                setUserToDos(data);
+            })
         }
     }
     return(
         <Fragment key={item.id}>
-        <div className='w-full border border-blue-500 rounded-md px-2 flex flex-col my-1'>
+        <div className='w-full border border-blue-500 rounded-xl px-2 flex flex-col my-1 shadow-xl'>
             <div className="flex flex-row divide-x">
+            <div className="my-auto bw-1/6">
             <input type='checkbox' value={item.id} name={item.id} onChange={checkedBox} className='ml-1' />
-            <div className="flex flex-col mx-auto">
-            <span className="text-xs text-gray-400 italic">{item.dueAt}</span>
+            </div>
+            <div className="flex flex-col mx-auto w-4/6 pl-1">
+            <span className="text-xs text-gray-400 italic">Due on {item.dueAt}</span>
             <p className="text-sm">{item.content}</p>
+            </div>
+            <div className="flex flex-row m-auto w-fit pl-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 text-gray-400 font-bold">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+            <img alt="Members of the task" src="https://xsgames.co/randomusers/avatar.php?g=male" className='rounded-full w-auto h-8' />
+            <img alt="Members of the task" src="https://xsgames.co/randomusers/avatar.php?g=female" className='rounded-full w-auto h-8 relative right-3' />
             </div>
             </div>
         </div>
